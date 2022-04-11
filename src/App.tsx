8f1259bf7f1937c55848b0541f9632e6ac536ae6
Harry Craven <harrycraven.dev@gmail.com>
2022-04-11 16:57:34 +0100

    inserting components to app file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { TypeItProvider } from "./context/TypeItContext";
 import DisplayWord from "./components/display-word/display.word.component";
+import WordInput from "./components/word-input/Word.input.component";
+import GameControls from "./components/game-controls/game.control.component";
+import { GameTime } from "./components/game-timer/game.timer.component";
 
 function App() {
 	return (
-		<div className="App">
+		<div className="App min-h-screen flex justify-center items-center flex-col">
 			<TypeItProvider>
 				<DisplayWord />
+				<WordInput />
+				<GameTime />
+				<GameControls />
 			</TypeItProvider>
 		</div>
 	);
